14f04dabb6498426f9a01169bd487bc88f3dc20d
sua <josuah49@gmail.com>
2023-08-08 11:12:40 +0900

    시간 처리 완료 ~60초까지 => 방금 전 ~60분까지 => ~분 전 ~24시간 까지 => HH:mm ~30일 이후 => MM/dd ~365일 이후 => YYYY/MM/dd

--- src/main/java/com/fp/eb/model/Time.java
+++ src/main/java/com/fp/eb/model/Time.java
@@ -1,8 +1,10 @@
 package com.fp.eb.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Time {
+
 	private static class TIME_MAXIMUM {
 		public static final int SEC = 60;
 		public static final int MIN = 60;
@@ -10,35 +12,31 @@ public class Time {
 		public static final int DAY = 30;
 		public static final int MONTH = 12;
 	}
-	
-	
-	public static String calculateTime(Date tempDate) {
+	public static String calculateTime(Date date) {
 		long curTime = System.currentTimeMillis();
-		System.out.println(curTime);
-		long regTime = tempDate.getTime();
-		System.out.println(regTime);
+		long regTime = date.getTime();
 		long diffTime = (curTime - regTime) / 1000;
 		String msg = null;
-		
-		
 		if (diffTime < TIME_MAXIMUM.SEC) {
 			// sec
 			msg = "방금 전";
 		} else if ((diffTime /= TIME_MAXIMUM.SEC) < TIME_MAXIMUM.MIN) {
 			// min
-			msg = diffTime + "분 전";
+			msg = diffTime + "분 전"; 
 		} else if ((diffTime /= TIME_MAXIMUM.MIN) < TIME_MAXIMUM.HOUR) {
 			// hour
-			msg = (diffTime) + "시간 전";
+			msg = new SimpleDateFormat("HH:mm").format(regTime);
 		} else if ((diffTime /= TIME_MAXIMUM.HOUR) < TIME_MAXIMUM.DAY) {
 			// day
-			msg = (diffTime) + "일 전";
+			msg = new SimpleDateFormat("MM/dd").format(regTime);
 		} else if ((diffTime /= TIME_MAXIMUM.DAY) < TIME_MAXIMUM.MONTH) {
-			// day
-			msg = (diffTime) + "달 전";
-		} else {
-			msg = (diffTime /= TIME_MAXIMUM.MONTH) + "년 전";
+			// month
+			msg = new SimpleDateFormat("MM/dd").format(regTime);
+		} else if((diffTime > TIME_MAXIMUM.MONTH)){
+			// year
+			msg = new SimpleDateFormat("YYYY/MM/dd").format(regTime);
 		}
 		return msg;
 	}
+	
 }
